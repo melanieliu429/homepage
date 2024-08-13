d91ca1a7a6b8f4506cf8c09ee7fd7e921461ac29
Melanie <melanieliu429@gmail.com>
2024-08-12 18:29:20 -0700

    added projects

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import phoneIcon from "./img/phone.svg";
 import emailIcon from "./img/email.svg";
 import footerMe from "./img/footerMe.jpg";
 
+import cvApplication from "./img/cv-application.png";
+import weatherApp from "./img/weather-app.png";
+import toDoList from "./img/to-do-list.png";
+import restaurantPage from "./img/restaurant-page.png";
+import ticTacToe from "./img/tic-tac-toe.png";
+import library from "./img/library.png";
+import adminDashboard from "./img/admin-dashboard.png";
+import signUpForm from "./img/sign-up-form.png";
+import calculator from "./img/calculator.png";
+import etchASketch from "./img/etch-a-sketch.png";
+import rockPaperScissors from "./img/rock-paper-scissors.png";
+import landingPage from "./img/landing-page.png";
+
 document.querySelectorAll('.headerMe').forEach(img => img.src = headerMe);
 document.querySelectorAll('.github').forEach(img => img.src = githubIcon);
 document.querySelectorAll('.linkedin').forEach(img => img.src = linkedinIcon);
@@ -18,3 +31,15 @@ document.querySelector('.footerMe').src = footerMe;
 document.querySelector('.phone').src = phoneIcon;
 document.querySelector('.email').src = emailIcon;
 
+document.querySelector('#cv-application').src = cvApplication;
+document.querySelector('#weather-app').src = weatherApp;
+document.querySelector('#to-do-list').src = toDoList;
+document.querySelector('#restaurant-page').src = restaurantPage;
+document.querySelector('#tic-tac-toe').src = ticTacToe;
+document.querySelector('#library').src = library;
+document.querySelector('#admin-dashboard').src = adminDashboard;
+document.querySelector('#sign-up-form').src = signUpForm;
+document.querySelector('#calculator').src = calculator;
+document.querySelector('#etch-a-sketch').src = etchASketch;
+document.querySelector('#rock-paper-scissors').src = rockPaperScissors;
+document.querySelector('#landing-page').src = landingPage;
